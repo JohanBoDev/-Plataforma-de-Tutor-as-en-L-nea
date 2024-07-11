fd70e86e22569e0205204046feb60c229abb4a61
JohanBoDev <bohorquezjohan958@gmail.com>
2024-07-11 10:28:32 -0500

    ...

--- ProyectoFinal/vite.config.js
+++ ProyectoFinal/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    host: true 
   }
 });
